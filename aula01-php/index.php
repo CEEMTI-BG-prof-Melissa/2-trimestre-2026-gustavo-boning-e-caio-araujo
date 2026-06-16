9ac02a924588bc5275e49ff7394288cafdb3fe67
gustavoboning <gustavo.oboning@aluno.edu.es.gov.br>
2026-06-16 11:07:33 -0300

    Create index.php

---
+++ aula01-php/index.php
@@ -0,0 +1,15 @@
+<DOCTYPE HHTML>
+    <<html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <title>Aula 01 html</title>
+        </head>
+        <body>
+            <?php
+        echo "Olá, Mundo!";
+        
+        ?>
+
+
+        </body>
+</DOCTYPE>
